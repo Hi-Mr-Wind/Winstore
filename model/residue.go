c3492ec3e4f1f8b08e39ee60de83990d7fcb5064
wind <wzsf1810@qq.com>
2025-09-01 16:46:01 +0800

    前端页面优化，完成收藏夹部分

--- model/residue.go
+++ model/residue.go
@@ -1,10 +1,15 @@
 package model
 
 import (
-	"encoding/json/v2"
 	"time"
 	"winstore/comm"
 )
+
+// 在init函数中注册Residue类型到gob
+func init() {
+	comm.RegisterType([]Residue{})
+	comm.RegisterType(Residue{})
+}
 
 // Residue 软件卸载后残余数据
 type Residue struct {
@@ -16,62 +21,57 @@ type Residue struct {
 
 // InsertData 插入残余软件数据
 func (residue *Residue) InsertData() error {
-	get, b := comm.AppCache.Get("residue")
 	var residueList []Residue
-	if !b {
-		residueList = append(residueList, *residue)
-	} else {
-		err := json.Unmarshal([]byte(get.(string)), &residueList)
-		if err != nil {
-			return err
+
+	// 从缓存中获取现有残余数据列表
+	if val, ok := comm.AppCache.Get("residue"); ok {
+		if cachedList, ok := val.([]Residue); ok {
+			residueList = cachedList
 		}
-		residueList = append(residueList, *residue)
 	}
-	marshal, err := json.Marshal(residueList)
-	if err != nil {
-		return err
-	}
-	comm.AppCache.Set("residue", string(marshal), 24*30*time.Hour)
+
+	// 添加新的残余数据项
+	residueList = append(residueList, *residue)
+
+	// 保存回缓存
+	comm.AppCache.Set("residue", residueList, 24*30*time.Hour)
 	return nil
 }
 
 // SelectAll 查询所有残余软件数据
 func (residue *Residue) SelectAll() *[]Residue {
 	var residueList []Residue
-	for key, value := range comm.AppCache.GetAll() {
-		if key != "residue" {
-			continue
-		}
-		value := value.(string)
-		err := json.Unmarshal([]byte(value), &residueList)
-		if err != nil {
-			return nil
+
+	// 从缓存中获取残余数据列表
+	if val, ok := comm.AppCache.Get("residue"); ok {
+		if cachedList, ok := val.([]Residue); ok {
+			residueList = cachedList
 		}
 	}
+
 	return &residueList
 }
 
 // DeleteByName 根据软件名称删除残余软件数据
 func (residue *Residue) DeleteByName() error {
-	get, b := comm.AppCache.Get("residue")
-	if !b {
-		return nil
-	}
 	var residueList []Residue
-	err := json.Unmarshal([]byte(get.(string)), &residueList)
-	if err != nil {
-		return err
+
+	// 从缓存中获取现有残余数据列表
+	if val, ok := comm.AppCache.Get("residue"); ok {
+		if cachedList, ok := val.([]Residue); ok {
+			residueList = cachedList
+		}
 	}
+
+	// 查找并删除指定名称的残余数据项
 	for i, v := range residueList {
 		if v.ResidueAppName == residue.ResidueAppName {
 			residueList = append(residueList[:i], residueList[i+1:]...)
 			break
 		}
 	}
-	marshal, err := json.Marshal(residueList)
-	if err != nil {
-		return err
-	}
-	comm.AppCache.Set("residue", string(marshal), 24*30*time.Hour)
+
+	// 保存更新后的列表回缓存
+	comm.AppCache.Set("residue", residueList, 24*30*time.Hour)
 	return nil
 }
